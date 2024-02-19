5f4e887db98317b9e603cbd338ba5407f5ba41e0
mj <wmj0924@gmail.com>
2024-02-19 12:41:14 +0900

    sout 사용

---
+++ src/Main3.java
@@ -0,0 +1,12 @@
+public class Main3 {
+    public static void main(String[] args) {
+        System.out.println("1");
+        System.out.println("2");
+        System.out.println("3");
+        System.out.println("4");
+        System.out.println("5식이");
+        System.out.println("djdhf");
+        System.out.println("이거이거 신기하네");
+
+    }
+}
